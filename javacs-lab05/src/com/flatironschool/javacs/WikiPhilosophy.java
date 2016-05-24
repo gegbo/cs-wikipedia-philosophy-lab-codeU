ef99520cb52186ad0473524efa1fdd62ca130fa2
Grace Egbo <grace.egbo1@gmail.com>
2016-05-24 11:17:38 -0500

    Completed lab

--- javacs-lab05/src/com/flatironschool/javacs/WikiPhilosophy.java
+++ javacs-lab05/src/com/flatironschool/javacs/WikiPhilosophy.java
@@ -11,41 +11,122 @@ import org.jsoup.nodes.TextNode;
 import org.jsoup.select.Elements;
 
 public class WikiPhilosophy {
-	
-	final static WikiFetcher wf = new WikiFetcher();
-	
-	/**
-	 * Tests a conjecture about Wikipedia and Philosophy.
-	 * 
-	 * https://en.wikipedia.org/wiki/Wikipedia:Getting_to_Philosophy
-	 * 
-	 * 1. Clicking on the first non-parenthesized, non-italicized link
-     * 2. Ignoring external links, links to the current page, or red links
-     * 3. Stopping when reaching "Philosophy", a page with no links or a page
-     *    that does not exist, or when a loop occurs
-	 * 
-	 * @param args
-	 * @throws IOException
-	 */
-	public static void main(String[] args) throws IOException {
-		
-        // some example code to get you started
 
-		String url = "https://en.wikipedia.org/wiki/Java_(programming_language)";
-		Elements paragraphs = wf.fetchWikipedia(url);
+        final static WikiFetcher wf = new WikiFetcher();
+        ArrayList<String> done = new ArrayList<String>();
+        int count = 0; //helps check if parentthesis are balanced
+        /**
+        * Tests a conjecture about Wikipedia and Philosophy.
+        *
+	* https://en.wikipedia.org/wiki/Wikipedia:Getting_to_Philosophy
+	*
+	* 1. Clicking on the first non-parenthesized, non-italicized link
+	* 2. Ignoring external links, links to the current page, or red links
+ * 3. Stopping when reaching "Philosophy", a page with no links or a page
+      *    that does not exist, or when a loop occurs
+               *
+	                * @param args
+			         * @throws IOException
+				          */
 
-		Element firstPara = paragraphs.get(0);
-		
-		Iterable<Node> iter = new WikiNodeIterable(firstPara);
-		for (Node node: iter) {
-			if (node instanceof TextNode) {
-				System.out.print(node);
-			}
-        }
+					          public static void testPhilosophy(String start, String end) {
+						                  String test = start;
+								                  for(int count = 0; i<50; i++)
+										                  {
+												                          if(done.contains(test)){
+															                                  return; //duplicate links (loops)
+																			                          }
+																						                          else
+																									                          {
+																												                                  done.add(test);
+																																                          }
 
-        // the following throws an exception so the test fails
-        // until you update the code
-        String msg = "Complete this lab by adding your code and removing this statement.";
-        throw new UnsupportedOperationException(msg);
-	}
-}
+																																			                          Element link = getGoodLink(start);
+																																						                          if(link == null) {
+																																									                                  return; //no links on page
+																																													                          }
+
+																																																                          if (link.attr("abs:href").equals(end)) {
+																																																			                                  break; //matches philosophy page
+																																																							                          }
+																																																										                  }
+																																																												          }
+
+
+																																																													          public static Element getGoodLink(String link) {
+																																																														                  Elements paragraphs = wf.fetchWikipedia(link);
+
+																																																																                  for (Element paragraph: paragraphs) {
+																																																																		                          Element firstLink = firstLinkInPara(paragraph);
+																																																																					                          if(firstLink!=null) {
+																																																																								                                  return firstLink;
+																																																																												                          }
+																																																																															                  }
+																																																																																	                  return null;
+																																																																																			          }
+																																																																																				          public static Element firstLinkInPara(Node head) {
+																																																																																					                  Iterable<Node> iterate = new WikiNodeIterable(head);
+
+																																																																																							                  for(Node node: head) {
+
+																																																																																									                          //if(node instanceof TextNode) {
+																																																																																												                          //      checkParen((TextNode)node);
+																																																																																															                          //}
+
+																																																																																																		                          if(node instanceof Element) {
+																																																																																																					                                  Element aLink = findLink((Element)node);
+																																																																																																									                                  if(aLink != null) {
+																																																																																																													                                          return aLink;
+																																																																																																																		                                  }
+																																																																																																																						                          }
+																																																																																																																									                  }
+																																																																																																																											                  return null;
+																																																																																																																													          }
+
+																																																																																																																														          public static Element findLink(Element element) {
+																																																																																																																															                  if(!element.tagName().equals("a")) { //symbol for a link
+																																																																																																																																	                          return null;
+																																																																																																																																				                  }
+																																																																																																																																						                  char[] parenthesis_text = (String)element.toCharArray();
+
+																																																																																																																																								                  for(char symbol: parenthesis_text)//checks if in parenthesis
+																																																																																																																																										                  {
+																																																																																																																																												                          if(symbol == '('){
+																																																																																																																																															                                  count++;
+																																																																																																																																																			                          }
+
+																																																																																																																																																						                          if(symbol == ')'){
+
+																																																																																																																																																									                                  count--;
+																																																																																																																																																													                          }
+																																																																																																																																																																                          if(count<0) { //string unbalanced from the start
+																																																																																																																																																																			                                  return null;
+																																																																																																																																																																							                          }
+
+																																																																																																																																																																										                          if(count != 0)
+																																																																																																																																																																													                          {
+																																																																																																																																																																																                                  return null;
+																																																																																																																																																																																				                          } 
+																																																																																																																																																																																							                 }
+
+																																																																																																																																																																																									                 for ( Element e = element; element !=null; e = element.parent())
+																																																																																																																																																																																											 {
+																																																																																																																																																																																											                         if(e.tagname().equals("i") || e.tagName().equals("em")){
+																																																																																																																																																																																														                                 return null;
+																																																																																																																																																																																																		                         }
+																																																																																																																																																																																																					                 }
+																																																																																																																																																																																																							                 //above checks if word is in italices
+																																																																																																																																																																																																									                 return element;
+
+																																																																																																																																																																																																											         }
+																																																																																																																																																																																																												         public static void main(String[] args) throws IOException {
+
+																																																																																																																																																																																																													                 String startUrl = "https://en.wikipedia.org/wiki/Washington_Univ
+																																																																																																																																																																																																															 ersity_in_St._Louis";
+
+																																																																																																																																																																																																															                 String finalUrl = "https://en.wikipedia.org/wiki/Philosophy";
+
+																																																																																																																																																																																																																	                 testPhilosophy(startUrl,finalUrl);
+
+																																																																																																																																																																																																																			         }
+																																																																																																																																																																																																																				 }
